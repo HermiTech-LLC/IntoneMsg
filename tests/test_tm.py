da352fc6200c71392aaaf814cb179fc561cea419
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-07-16 15:10:07 -0400

    Update test_tm.py

--- tests/test_tm.py
+++ tests/test_tm.py
@@ -1,6 +1,11 @@
 import os
+import sys
 import pytest
-from main import generate_tone, read_tone
+
+# Add the root directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from Tm import generate_tone, read_tone
 
 @pytest.fixture
 def test_wav_file():
